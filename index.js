a784ec191812c4fa9d64c6fbb0b05df43154a0e7
Shelli Martinez <shelli.mrtnz@gmail.com>
2023-11-05 06:57:17 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return window.customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+
+}
+
+const leastFavoriteCustomer = 'steve'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'lois';
+
+}
